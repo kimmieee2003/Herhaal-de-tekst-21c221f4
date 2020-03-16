c37020a61e3d272997f7022c8008e28b454ca702
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-16 12:09:22 +0100

    Uitwerking van de exercise

---
+++ variabele.php
@@ -0,0 +1,9 @@
+<?php
+
+$tekst = "Kimberly";
+echo "mijn naam is $tekst \n";
+echo "mijn naam is $tekst \n";
+echo "mijn naam is $tekst \n";
+echo "mijn naam is $tekst \n";
+echo "mijn naam is $tekst \n";
+?>
